5fbd9f24a779992deef10fb51d09247c4f41208d
Roman Opalacz <roman9891@gmail.com>
2020-05-31 16:32:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,9 +2,17 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   # write your code here!
+  reorganized_hash = {}
   names = []
+  
   data.each {|key, value| value.each {|key2, value2| value2.each {|x| names << x}}}
+  
   names = names.uniq
+  
+  names.each {
+  #for each name check to   
+  }
+  
   binding.pry
-  {}
+  reorganized_hash
 end
