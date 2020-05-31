5b44cb688aed2da84f966dee6ee4b659e6e73ce8
Roman Opalacz <roman9891@gmail.com>
2020-05-31 16:19:37 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,7 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
   names = []
-  data.each {|key, value| value.each {|name| puts name}}
+  data.each {|key, value| value.each {|name| names << name}}
   binding.pry
   {}
 end
