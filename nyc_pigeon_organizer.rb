a026167b6226499910dcedc4a0d433612272aaac
Roman Opalacz <roman9891@gmail.com>
2020-05-31 16:24:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,6 +4,7 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   names = []
   data.each {|key, value| value.each {|key2, value2| value2.each {|x| names << x}}}
+  names = names.uniq
   binding.pry
   {}
 end
